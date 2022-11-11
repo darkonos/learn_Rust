7c45d5f47131290fb410088034b87be75ae09158
darkonos <fred9.dev@gmail.com>
2022-11-11 17:30:12 +0400

    Fist commit

---
+++ learnRust/target/debug/deps/learnRust-2ec6073da756b30b.d
@@ -0,0 +1,5 @@
+d:\0.Repository\learn_Rust\learnRust\target\debug\deps\learnRust-2ec6073da756b30b.rmeta: src\main.rs
+
+d:\0.Repository\learn_Rust\learnRust\target\debug\deps\learnRust-2ec6073da756b30b.d: src\main.rs
+
+src\main.rs:
---
+++ learnRust/target/debug/deps/learnRust-6a93c81a283da410.d
@@ -0,0 +1,5 @@
+d:\0.Repository\learn_Rust\learnRust\target\debug\deps\learnRust-6a93c81a283da410.rmeta: src\main.rs
+
+d:\0.Repository\learn_Rust\learnRust\target\debug\deps\learnRust-6a93c81a283da410.d: src\main.rs
+
+src\main.rs:
